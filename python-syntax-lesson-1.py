e026ab922430e3b02ee810c0b164d03d7ec18978
marievanderpool <marievanderpool2@gmail.com>
2024-04-09 11:02:37 -0500

    first commit

--- python-syntax-lesson-1.py
+++ python-syntax-lesson-1.py
@@ -1,1 +1,83 @@
 #lesson-1-python
+
+#1.) Python Indentation Practice
+
+#task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+   print("Take an umbrella!")
+
+
+#task 2
+
+mood_a = 'happy'
+mood_b = 'sad'
+
+if mood_a == "happy":
+    print("That's great to hear!")
+if mood_b == "sad":
+    print ("I hope your day gets better!")
+
+
+#2.) Python Naming Convention Practice
+
+#task 1
+PI_VALUE = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+
+
+#3.) Python Data Types
+
+variable_a = "Hello, World!"
+if variable_a == "Hello, World!":
+    print ("this is a string")
+
+variable_b = 23
+
+if variable_b == 23:
+    print ("This is an Integer")
+
+variable_c = 3.14
+
+if variable_c == 3.14:
+    print("This is a float")
+
+variable_d = True
+
+if variable_d == True:
+    print("this is a boolean")
+
+
+#4.) Arithmetic Operations in Daily Life
+
+#Task 1
+bread = 2.50
+peanut_butter = 4
+jelly = 3.50
+
+sum = bread + peanut_butter + jelly
+print(sum)
+
+
+#Task 2
+prod = 10000 * .07
+print(prod)
+sum = 10000 + prod
+print(sum)
+
+#5.) Exploring Logical Operations & Precendence
+
+#Task 1
+
+sum = 5 + 5
+print (sum)
+
+sum = (1+1) * 5
+print (sum)
